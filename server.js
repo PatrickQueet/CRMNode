7e0bbd752de6bea8d5d99a56fb2c5dc837b77395
PatrickQueet <patrickqueet@gmail.com>
2020-10-08 07:30:54 +1200

    changed process.env.PORT

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.put('/image', (req, res) => {
 
 })
 
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, ()=> {
 	console.log('App is running on port 3001');
 })
